6f8e54edde72756bcbb2977f8320dc5e272243be
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2025-04-06 17:41:36 -0300

    Update langchain_pipeline.py

--- langchain_pipeline.py
+++ langchain_pipeline.py
@@ -4,11 +4,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableMap
 import os
-
-api_key = "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"  # Ou substitua diretamente pela chave
-
-
-llm = OpenAI(temperature=0.7, api_key=api_key)
+os.environ["OPENAI_API_KEY"] = "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"  # Ou substitua diretamente pela chave
+llm = OpenAI(temperature=0.7)
 
 # Prompts
 step_one = ChatPromptTemplate.from_template("Translate the following resume to English:\n\n{Resume}") | llm | StrOutputParser()
